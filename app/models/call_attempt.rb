43ba89fed52add12e72836802d7b9f475ba372d3
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-04-19 18:04:52 -0300

    Improve validation of call attempt

--- app/models/call_attempt.rb
+++ app/models/call_attempt.rb
@@ -6,6 +6,28 @@ class CallAttempt < ApplicationRecord
   belongs_to :phone
   belongs_to :user
   validates :outcome, presence: true, inclusion: { in: OUTCOMES }
-  validates :gender, inclusion: { in: GENDERS, allow_nil: true }
+  validates :gender, inclusion: { in: GENDERS, allow_nil: true, allow_blank: true }
   validates :user, presence: true
+
+  validate :validate_contacted_fields
+
+  def contacted?
+    outcome.to_s == 'contacted'
+  end
+
+  private
+
+  def validate_contacted_fields
+    unless contacted?
+      return
+    end
+
+    unless gender.present?
+      errors.add(:gender, :blank)
+    end
+
+    unless notes.present?
+      errors.add(:notes, :blank)
+    end
+  end
 end
