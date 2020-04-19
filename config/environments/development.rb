f504f3accf0b2bed088521657a267d2ddc77a41b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-04-18 21:11:51 -0300

    Disable host check for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # https://stackoverflow.com/questions/53878453/upgraded-rails-to-6-getting-blocked-host-error
+  config.hosts = nil
 end
