e09502ddf5ac65e86ebe9379bfeae90c5dba527c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-04-21 11:06:30 -0300

    Sort call attempts by created_at
    * Closes #35

--- app/models/call_attempt.rb
+++ app/models/call_attempt.rb
@@ -5,6 +5,9 @@ class CallAttempt < ApplicationRecord
   GENDERS = %w[male female].freeze
   belongs_to :phone
   belongs_to :user
+
+  default_scope -> { order(created_at: :asc) }
+
   validates :outcome, presence: true, inclusion: { in: OUTCOMES }
   validates :gender, inclusion: { in: GENDERS, allow_nil: true, allow_blank: true }
   validates :user, presence: true
