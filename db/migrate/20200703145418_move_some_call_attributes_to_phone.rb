969d5fb41e0dd811b48922d63f6c8db02bb01939
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-07-03 19:29:18 -0300

    Rename migration

--- db/migrate/20200703145418_move_some_call_attributes_to_phone.rb
+++ db/migrate/20200703145418_move_some_call_attributes_to_phone.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddNameAndGenderToPhone < ActiveRecord::Migration[6.0]
+class MoveSomeCallAttributesToPhone < ActiveRecord::Migration[6.0]
   def change
     add_column :phones, :name, :string
     add_column :phones, :gender, :string
